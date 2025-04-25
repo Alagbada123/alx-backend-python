131a77169c27a0ba304f9dffe34e7335ad6e9c3a
Abiodun <abiodunalagbada@gmail.com>
2025-04-25 15:38:22 +0100

    These

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
-'''Task 10's module.
-'''
-from typing import Any, Sequence, Union
+
+"""
+Guess I couldn't have done it without a docstring now could I?
+"""
+
+from typing import Sequence, Any, Union
+
+lst_type = Sequence[Any]
+return_type = Union[Any, None]
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    '''Retrieves the first element of a sequence if it exists.
-    '''
+def safe_first_element(lst: lst_type) -> return_type:
+    """
+    Get the first element in a sequence safely
+    If the there's no elements, return None
+    Args:
+        lst (list): A list of elements of any type
+    Returns:
+        if  element != None; Return first element
+        else return None
+    """
     if lst:
         return lst[0]
     else:
         return None
-
